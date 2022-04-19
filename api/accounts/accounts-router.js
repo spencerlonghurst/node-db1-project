c26eb4f9fa29acf33b112d212ec6c42cf1b71d60
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-04-18 21:41:24 -0600

    built out skeleton and started the users-models

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,59 @@
 const router = require('express').Router()
 
+const accountUser = require('./accounts-model');
+
+const midware = require('./accounts-middleware')
+
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  accountUser.get()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(next)
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', midware.checkAccountId, (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', midware.checkAccountPayload, midware.checkAccountNameUnique, (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.put('/:id', midware.checkAccountId, midware.checkAccountNameUnique, midware.checkAccountPayload, (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', midware.checkAccountId, (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err)
+  }
 })
 
+
+
+
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    // customMessage: 'Uh Oh, problem!',
+    message: err.message,
+    // stack: err.stack
+  })
 })
+
 
 module.exports = router;
